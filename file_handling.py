39c339ac9d0e28a706293792cd2ce362ede9775d
Stark <>
2019-12-12 21:04:07 +1100

    -- comment the file_handling.py -- add the price_data.csv into the project

--- file_handling.py
+++ file_handling.py
@@ -1,22 +1,47 @@
 #function record the data into a csv file named price_data.csv
 import csv
+import datetime
 
 #current method is not extendable 
 def record_price(price_dic):
     try:
-        with open('price_data.csv', 'w') as csv_file:
-            myFields = ['Camera model','Georges','DigiDirect','Teds','CameraPro']
+        with open('price_data.csv', 'a') as csv_file:
+            myFields = ['Date','Camera model','Georges','DigiDirect','Teds','CameraPro']
             csv_writer = csv.DictWriter(csv_file, fieldnames=myFields)
-            csv_writer.writeheader()
-            for x,y in price_dic.items():
-                csv_writer.writerow({'Camera model': x,
-                                     'Georges': y[0][1],
-                                     'DigiDirect':y[1][1],
-                                     'Teds': y[2][1],
-                                     'CameraPro': y[3][1]})
+            if not has_header('price_data.csv'):
+                csv_writer.writeheader()
+            if get_date('price_data.csv') != False:
+                csv_writer.writerow({'Date': get_date('price_data.csv')})
+                for model, price in price_dic.items():
+                    csv_writer.writerow({'Camera model': model,
+                                        'Georges': price[0][1],
+                                        'DigiDirect':price[1][1],
+                                        'Teds': price[2][1],
+                                        'CameraPro': price[3][1]})
     except IOError:
         print("File not accessible")
 
+#avoid adding redundent information based on the fact that the price won't change twice in a day
+def get_date(file):
+    x = datetime.datetime.now()
+    date = x.strftime('%x')
+    with open(file,'r') as csv_file:
+        for row in csv_file:
+            if date in row:
+                return False
+
+    return date
+
+#avoid adding redundent header
+def has_header(file):
+    header = ['Date','Camera model','Georges','DigiDirect','Teds','CameraPro']
+    with open(file,'r') as csv_file:
+        for row in csv_file:
+            if header[0] in row:
+                return True
+    return False
+
+#get the url information from the url_data.csv
 def get_url():
     try:
         price_dic={}
@@ -26,5 +51,5 @@ def get_url():
                 price_dic[row['Product_Number']] = [row['Product_Name'],row['Georges_url'],row['DigiDirect_url'],row['Teds_url'],row['CameraPro_url']]
             return price_dic
     except IOError:
-        print("File not accessible")
+        print("url not accessible")
 
